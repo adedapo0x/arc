b883bfdf98ba96d440ac8be0bc3c6b5a649fab83
Adedapo Adeyemi <adedapo025@gmail.com>
2025-03-11 14:40:53 +0100

    implemented repo_create and configuration file setup

--- libarc.py
+++ libarc.py
@@ -42,7 +42,7 @@ def main(argv=sys.argv[1:]):
 
 
 # An object to model the git repository
-class GitRepository(object):
+class GitRepository:
     worktree = None
     gitdir = None
     conf = None
@@ -100,7 +100,52 @@ def repo_file(repo, *path, mkdir=False):
         return repo_path(repo, *path)
     
 
+# Function to create new repository
+def repo_create(path):
+    '''Create new repository at path'''
+    repo = GitRepository(path, True)
+    
+    # Making sure path either doesn't exists or is an empty dir
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception(f"{path} is not a directory")
+        # check if gitdir exists andif it is empty
+        if not os.path.exists(repo.gitdir) and os.listdir(repo.gitdir): 
+            raise Exception(f"{path} is not empty")
+        
+    else:
+        os.makedirs(repo.worktree)
 
 
+    # if repo_dir returns None or empty string (indicating failure) and AssertionError is raised
+    assert repo_dir(repo, "branches", mkdir=True)
+    assert repo_dir(repo, "objects", mkdir=True)
+    assert repo_dir(repo, "refs", "tags", mkdir=True)
+    assert repo_dir(repo, "refs", "heads", mkdir=True)
 
 
+    #.git/description
+    with open(repo_file(repo, "description"), "w") as f:
+        f.write("Unnamed repository, edit this file 'description' to name the repository.\n")
+
+    #.git/HEAD
+    with open(repo_file(repo, "HEAD"), "w") as f:
+        f.write("ref: refs/heads/master\n")
+
+    with open(repo_file(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
+
+    return repo
+
+
+# Configuration file setup
+def repo_default_config():
+    ret = configparser.ConfigParser()
+
+    ret.add_section("core")
+    ret.set("core", "repositoryformatversion", "0")
+    ret.set("core", "filemode", "false")
+    ret.set("core", "bare", "false")
+
+    return ret
